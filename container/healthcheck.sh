cf23f4ec6ca78c91a7edb1646ca0382341f25673
Björn Petersen <git@bjoernpetersen.net>
2024-12-28 14:16:06 +0100

    Use single quotes

--- container/healthcheck.sh
+++ container/healthcheck.sh
@@ -4,7 +4,7 @@ set -eux -o pipefail
 
 content_type=$(curl -sSI -o /dev/null -w '%{content_type}' -H 'Accept: application/json' 'http://localhost:1337/api/v1/status')
 
-if [[ "$content_type" == "application/json; charset=utf-8" ]]
+if [[ "$content_type" == 'application/json; charset=utf-8' ]]
 then
   exit 0
 else
