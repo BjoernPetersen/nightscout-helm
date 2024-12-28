8642895a161fd7187880bf6706b05d815f51ada8
Björn Petersen <git@bjoernpetersen.net>
2024-12-28 14:15:53 +0100

    Fix healthcheck

--- container/healthcheck.sh
+++ container/healthcheck.sh
@@ -2,9 +2,9 @@
 
 set -eux -o pipefail
 
-content_type=$(curl -sSI -w '%{content_type}' -H 'Accept: application/json' 'http://localhost:1337/api/v1/status')
+content_type=$(curl -sSI -o /dev/null -w '%{content_type}' -H 'Accept: application/json' 'http://localhost:1337/api/v1/status')
 
-if [[ "$content_type" == "application/json" ]]
+if [[ "$content_type" == "application/json; charset=utf-8" ]]
 then
   exit 0
 else
